49b3078239cdc3aa242aa6434c3e28bfda277b54
Phuong Tran Thu <phuongtranbka@gmail.com>
2022-07-16 14:59:19 +0700

    Add Teamplate

---
+++ selenium/webdriver/Topic_00_Teamplate.java
@@ -0,0 +1,42 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Teamplate {
+
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("https://www.facebook.com/");	
+	}
+
+	@Test
+	public void TC_02() {
+		
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
