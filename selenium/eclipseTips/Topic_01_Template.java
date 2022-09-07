c690323648b5e445e777d5d6de887e6a3ad4df6d
Phuong <phuongtranbka@gmail.com>
2022-09-07 22:34:46 +0700

    eclipse tips

---
+++ selenium/eclipseTips/Topic_01_Template.java
@@ -0,0 +1,10 @@
+package eclipseTips;
+
+public class Topic_01_Template {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ selenium/eclipseTips/Topic_02_Debug.java
@@ -0,0 +1,26 @@
+package eclipseTips;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Topic_02_Debug {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@Test
+	public void TC_01() {
+		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		String headerText = driver
+				.findElement(By.xpath("" + "//img[@alt='Facebook']/parent::div/following-sibling::h2")).getText();
+		Assert.assertEquals(headerText, "Facebook helps you connect and share with the people in your life.");
+
+		boolean loginStatus = driver.findElement(By.name("login")).isDisplayed();
+		Assert.assertTrue(loginStatus);
+	}
+
+}
